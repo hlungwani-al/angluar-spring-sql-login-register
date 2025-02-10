7916c521937c4a9edc74d3ac2e36246b1bd5b0e6
hlungwani-al <hlungwani.al6@gmail.com>
2025-02-10 15:52:01 +0200

    working from here

--- reg-login/src/app/pages/register/register.component.ts
+++ reg-login/src/app/pages/register/register.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { UserService } from '../../user.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -19,11 +19,14 @@ export class RegisterComponent {
     role: 'BUYER'
   };
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   register() {
     this.userService.registerUser(this.user).subscribe(response => {
       console.log('User registered successfully', response);
+      localStorage.setItem('userId', response.id.toString());
+      // Redirect to the profile page after successful login
+      this.router.navigate(['/profile']);
     });
   }
 }
